fb0dfb4f8950cdf4c4c4bd1f268dee08a93212f9
bailooon <matheus@bailon.com.br>
2025-08-12 20:17:49 -0300

    criando as screens

---
+++ android/app/src/main/kotlin/br/com/bailooon/obras_de_arte/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.bailooon.obras_de_arte
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/detail/artwork_detail_screen.dart
@@ -0,0 +1,15 @@
+class ArtworkDetailScreen extends StatefulWidget {
+
+  final Artwork artwork;
+  const ArtworkDetailScreen({super.key, required this.artwork});
+  @override
+  State<ArtworkDetailScreen> createState() => _ArtworkDetailScreenState();
+}
+
+class _ArtworkDetailScreenState extends State<ArtworkDetailScreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
